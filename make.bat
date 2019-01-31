2986082d6227d5a4b899da6efbbccb4fcbc677bb
Farzin Mohammadi <farzinlize@live.com>
2019-01-31 18:26:45 +0330

    improve make.bat to add defines as arguments and sources as a local variable

--- make.bat
+++ make.bat
@@ -1,13 +1,23 @@
 @echo off
+SETLOCAL
+SET source=kernel.cu helper_functions.c fuzzy_timing.c
+SET __defines=
+if "%~1"=="" goto :command
 if "%1" == "clean" goto :clean
-if "%1" == "debug" goto :debug
 
-nvcc -o add_cuda.exe kernel.cu helper_functions.c fuzzy_timing.c --gpu-architecture=sm_35 -rdc=true
-goto :eof
+:loop_args
+if "%1" == "debug" set __defines=%__defines% -DDEBUG
+if "%1" == "overlap" set __defines=%__defines% -DOVERLAP
+shift
+if NOT "%~1"=="" goto :loop_args
 
-:debug
-nvcc -o add_cuda_debug.exe kernel.cu helper_functions.c fuzzy_timing.c -D DEBUG
+:command
+echo %source% %__defines%
+nvcc -o add_cuda.exe %source% %__defines%
+ENDLOCAL
 goto :eof
 
 :clean
+echo clean
 del add_cuda.exe add_cuda.exp add_cuda.lib add_cuda_debug.exe add_cuda_debug.exp add_cuda_debug.lib
+ENDLOCAL
